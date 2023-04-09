349fcf0d986e238a1a73193fbe830ecb3f159279
maicondguerian <maicondguerian@gmail.com>
2023-04-09 15:24:28 -0300

    fixest

--- src/components/myProjects/Projects.jsx
+++ src/components/myProjects/Projects.jsx
@@ -70,34 +70,40 @@ const InfosWrapper = styled.span`
   }
 `;
 
+const List = ({ mock }) => {
+  const [modal, setModal] = useState(false);
+  return (
+    <InfosWrapper key={mock.id}>
+      <ul>
+        <li
+          onMouseEnter={()=> setModal(true)}
+          onMouseLeave={() => setModal(false)}
+        >
+          {mock.img}
+          <Modal isOpen={modal} />
+        </li>
+      </ul>
+      <span>{mock.proj}</span>
+      <p> {mock.exp} </p>
+    </InfosWrapper>
+  )
+}
+
 export const Projects = () => {
 
   const projectsList = [
     { proj: "dev-finder-github-api", exp: "React fetchAPI react-icons", img: <img src={pjt1} alt="dev-finderGitHubAPI" />, id: 1 },
-    { proj: "roll-adive-advisor", exp: "React fetchAPI react-icons sass", img: <img src={pjt2} alt="roll-adive-advisor" /> },
-    { proj: "interactive-rating-component", exp: "React sass Semantic html5 markup", img: <img src={pjt3} alt="roll-adive-advisor" /> },
-    { proj: "React", exp: "exp" },
-    { proj: "Next-js", exp: "exp" },
-    { proj: "Jquery", exp: "exp" },
+    { proj: "roll-adive-advisor", exp: "React fetchAPI react-icons sass", img: <img src={pjt2} alt="roll-adive-advisor"/>, id: 2  },
+    { proj: "interactive-rating-component", exp: "React sass Semantic html5 markup", img: <img src={pjt3} alt="roll-adive-advisor" />, id: 3},
+    { proj: "React", exp: "exp",  id: 4 },
+    { proj: "Next-js", exp: "exp",  id: 5 },
+    { proj: "Jquery", exp: "exp", id: 6 },
   ];
 
   const SetThumbnail = (e) => {
-    const { isOpen, handleShowModal, setIsOpen } = useContext(MyContext);
-
-    return projectsList.map((mock, index) => (
-      <InfosWrapper key={index}>
-        <ul>
-          <li
-            onMouseEnter={handleShowModal}
-            onMouseLeave={() => setIsOpen(false)}
-          >
-            {mock.img}
-            <Modal isOpen={isOpen} />
-          </li>
-        </ul>
-        <span>{mock.proj}</span>
-        <p> {mock.exp} </p>
-      </InfosWrapper>
+    // const { isOpen, handleShowModal, setIsOpen } = useContext(MyContext);
+    return projectsList.map((mock) => (
+      <List mock={mock}/>
     ));
   };
 
