af3afc042c6b4d012f7128346da06ffee8676ede
maicondguerian <maicondguerian@gmail.com>
2023-04-14 15:04:43 -0300

    fixing erros in Environments

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portifolio',
   build: {
     rollupOptions: {
       external: ['react-icons/bs']
